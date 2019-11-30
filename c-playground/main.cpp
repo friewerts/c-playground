b0cd5a0f0c251d70b09b05444a46d2da59180263
Frederik Riewerts <frederik.riewerts@gmail.com>
2019-11-30 21:37:44 +0100

    added user input

--- c-playground/main.cpp
+++ c-playground/main.cpp
@@ -12,13 +12,43 @@
 
 using namespace std;
 
+bool validateInput(string input) {
+  bool isValid = true;
+
+  for (char const &c: input) {
+    isValid &= (c == '0' || c == '1');
+  }
+
+  return isValid;
+}
+
+string getSequence() {
+  string userInput;
+  
+  cout << "Enter Sequence:";
+  cin >> userInput;
+  
+  if (validateInput(userInput)) {
+    cout << "Input valid!" << endl;
+    return userInput;
+  }
+  else {
+    cout << "Input invalid! Try again only using the digits 0 and 1." << endl;
+    return getSequence();
+  }
+}
+
 int main(int argc, const char * argv[]) {
 
-  Automat("2a").getState("0011");
-  Automat("2b").getState("0011110011010");
-  Automat("2c").getState("01100100101");
-  Automat("2d").getState("001111000011101");
-  Automat("2e").getState("110110101010010110100");
+//  Automat("2a").getState("0011");
+//  Automat("2b").getState("0011110011010");
+//  Automat("2c").getState("01100100101");
+//  Automat("2d").getState("001111000011101");
+//  Automat("2e").getState("110110101010010110100");
 
+  string userInput = getSequence();
+  
+  Automat().getState(userInput);
+  
   return 0;
 }
